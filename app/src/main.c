2c02247c330b08280c16d10f13629cdf4cf7fd50
Eve <eve.redero@gmail.com>
2023-11-10 13:36:41 +0100

    first commit

--- app/src/main.c
+++ app/src/main.c
@@ -4,45 +4,21 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
-#include <app_version.h>
+#include <zephyr/devicetree.h>
 
+#include <app/drivers/wireless/nrf24l01.h>
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 int main(void)
 {
 	int ret;
-	const struct device *sensor;
+	static const struct device *nrf24 = DEVICE_DT_GET(DT_NODELABEL(radio0));
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
-
-	sensor = DEVICE_DT_GET(DT_NODELABEL(examplesensor0));
-	if (!device_is_ready(sensor)) {
+	if (!device_is_ready(nrf24)) {
 		LOG_ERR("Sensor not ready");
 		return 0;
 	}
-
-	while (1) {
-		struct sensor_value val;
-
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return 0;
-		}
-
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return 0;
-		}
-
-		printk("Sensor value: %d\n", val.val1);
-
-		k_sleep(K_MSEC(1000));
-	}
-
 	return 0;
 }
 
