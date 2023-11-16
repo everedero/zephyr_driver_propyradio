1462ba3bb73b905dceb441e6c75a4c3c199c5607
Eve <eve.redero@gmail.com>
2023-11-16 15:46:09 +0100

    wip: fix issue with spi structure, has to be declared as struct not struct *

--- drivers/nrf24l01/nrf24l01.c
+++ drivers/nrf24l01/nrf24l01.c
@@ -14,10 +14,9 @@
 LOG_MODULE_REGISTER(nrf24l01, CONFIG_NRF24L01_LOG_LEVEL);
 
 struct nrf24l01_config {
-	const struct device *spi;
+	const struct spi_dt_spec spi;
 	struct gpio_dt_spec ce;
 	struct gpio_dt_spec irq;
-
 };
 
 struct nrf24l01_data {
@@ -74,38 +73,44 @@ static int nrf24l01_init(const struct device *dev)
 		return ret;
 	}
 
+	ret = nrf24l01_set_channel(dev);
+
 	return 0;
 }
 
+#define NRF24L01_SPI_MODE   (SPI_WORD_SET(8) | SPI_TRANSFER_MSB)
+
 #define NRF24L01_DEFINE(i)                                             \
 	static const struct nrf24l01_config nrf24l01_config_##i = {        \
-		.spi = DEVICE_DT_GET(DT_INST_BUS(i)),                         \
-		.ce = GPIO_DT_SPEC_INST_GET_OR(i, ce_gpios, {}),                              \
-		.irq = GPIO_DT_SPEC_INST_GET_OR(i, irq_gpios, {}),                              \
-	};                                                                     \
-                                                                               \
-	static struct nrf24l01_data nrf24l01_##i = {                            \
-		.addr_width = DT_INST_PROP(i, addr_width),                            \
-		.channel_frequency = DT_INST_PROP(i, channel_frequency),                            \
-		.data_rate_2mbps = DT_INST_PROP_OR(i, data_rate_2mbps, false),                            \
-		.rf_power_attenuation = DT_INST_PROP(i, rf_power_attenuation),                            \
-		.lna_gain = DT_INST_PROP_OR(i, lna_gain, false),                            \
-		.crc_encoding_twobytes = DT_INST_PROP_OR(i, crc_encoding_twobytes, false),                            \
-		.tx_address = DT_INST_PROP(i, tx_address),                            \
-		.dynamic_payload = DT_INST_PROP_OR(i, dynamic_payload, false),                            \
-		.payload_ack = DT_INST_PROP_OR(i, payload_ack, false),                            \
-		.rx_datapipes_number = DT_INST_PROP(i, payload_ack),                            \
-		.rx_datapipe0_address = DT_INST_PROP(i, rx_datapipe0_address),                            \
-		.rx_datapipe1_address = DT_INST_PROP(i, rx_datapipe1_address),                            \
-		.rx_datapipe2_address = DT_INST_PROP(i, rx_datapipe2_address),                            \
-		.rx_datapipe3_address = DT_INST_PROP(i, rx_datapipe3_address),                            \
-		.rx_datapipe4_address = DT_INST_PROP(i, rx_datapipe4_address),                            \
-		.rx_datapipe5_address = DT_INST_PROP(i, rx_datapipe5_address),                            \
-		.rx_datapipes_dynamic_payload = DT_INST_PROP_OR(i, rx_datapipes_dynamic_payload, {}),                            \
-		.rx_datapipes_fixed_size_payload = DT_INST_PROP_OR(i, rx_datapipes_fixed_size_payload, {}),                            \
-	};                                                                     \
-	DEVICE_DT_INST_DEFINE(i, nrf24l01_init, NULL, NULL,                  \
-			      &nrf24l01_config_##i, POST_KERNEL,             \
+		.spi = SPI_DT_SPEC_INST_GET(i, NRF24L01_SPI_MODE, 0),           \
+		.ce = GPIO_DT_SPEC_INST_GET_OR(i, ce_gpios, {}),               \
+		.irq = GPIO_DT_SPEC_INST_GET_OR(i, irq_gpios, {}),             \
+	};                                                                            \
+                                                                                  \
+	static struct nrf24l01_data nrf24l01_##i = {                                  \
+		.addr_width = DT_INST_PROP(i, addr_width),                                \
+		.channel_frequency = DT_INST_PROP(i, channel_frequency),                  \
+		.data_rate_2mbps = DT_INST_PROP_OR(i, data_rate_2mbps, false),            \
+		.rf_power_attenuation = DT_INST_PROP(i, rf_power_attenuation),            \
+		.lna_gain = DT_INST_PROP_OR(i, lna_gain, false),                          \
+		.crc_encoding_twobytes = DT_INST_PROP_OR(i, crc_encoding_twobytes, false),\
+		.tx_address = DT_INST_PROP(i, tx_address),                                \
+		.dynamic_payload = DT_INST_PROP_OR(i, dynamic_payload, false),            \
+		.payload_ack = DT_INST_PROP_OR(i, payload_ack, false),                    \
+		.rx_datapipes_number = DT_INST_PROP(i, payload_ack),                      \
+		.rx_datapipe0_address = DT_INST_PROP(i, rx_datapipe0_address),            \
+		.rx_datapipe1_address = DT_INST_PROP(i, rx_datapipe1_address),            \
+		.rx_datapipe2_address = DT_INST_PROP(i, rx_datapipe2_address),           \
+		.rx_datapipe3_address = DT_INST_PROP(i, rx_datapipe3_address),            \
+		.rx_datapipe4_address = DT_INST_PROP(i, rx_datapipe4_address),            \
+		.rx_datapipe5_address = DT_INST_PROP(i, rx_datapipe5_address),            \
+		.rx_datapipes_dynamic_payload = DT_INST_PROP_OR(i,                        \
+				rx_datapipes_dynamic_payload, {}),                                \
+		.rx_datapipes_fixed_size_payload = DT_INST_PROP_OR(i,                     \
+				rx_datapipes_fixed_size_payload, {}),                             \
+	};                                                                            \
+	DEVICE_DT_INST_DEFINE(i, nrf24l01_init, NULL, &nrf24l01_##i,  \
+			      &nrf24l01_config_##i, POST_KERNEL,              \
 			      CONFIG_NRF24L01_INIT_PRIORITY, &nrf24l01_api);
 
 DT_INST_FOREACH_STATUS_OKAY(NRF24L01_DEFINE)
