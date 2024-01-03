c56e9de7f559692ff9f4477b8c74a93d077c6e88
Eve <eve.redero@gmail.com>
2024-01-03 23:06:03 +0100

    wip: main script change to have 3 different behaviours

--- app/src/main.c
+++ app/src/main.c
@@ -13,36 +13,69 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 #error "whoops, node label radio0 not found"
 #endif
 
-//#define IS_TX
-
 int main(void)
 {
 	static const struct device *nrf24 = DEVICE_DT_GET(DT_NODELABEL(radio0));
-	uint8_t data_len = 8;
-	uint8_t buffer[] = {0xab, 0xcd, 0xef, 0xaa, 0xbb, 0xcc, 0xdd, 0xee};
+	uint8_t data_len = 16;
+	uint8_t buffer[16] = {0};
+	int ret;
 
 	if (!device_is_ready(nrf24)) {
 		LOG_ERR("Sensor not ready");
 		return 0;
 	}
 	LOG_INF("Device ready");
-#ifdef IS_TX
-	LOG_INF("I am TX!");
+
+#if CONFIG_ROLE == 0
+	#define ALICE
+	printk("I am Alice!\n");
+#elif CONFIG_ROLE == 1
+	#define BOB
+	printk("I am Bob!\n");
 #else
-	LOG_INF("I am RX");
+	#define EVE
+	printk("I am Eve!\n");
 #endif
 
-#ifdef IS_TX
+#ifdef ALICE
 	while (true) {
-		nrf24_write(nrf24, buffer, data_len);
-		LOG_INF("Wrote stuff");
-		k_sleep(K_MSEC(1000));
-	}
-#else
-	while (true) {
-		nrf24_read(nrf24, buffer, data_len);
-		k_sleep(K_MSEC(10));
+		strncpy(buffer, "I am Alice, hi!", 16);
+		while (true) {
+			ret = nrf24_write(nrf24, buffer, data_len);
+			if (ret != 0) {
+				printk("Not received\n");
+			} else {
+				printk("Ret: %d\n", ret);
+				break;
+			}
+			k_sleep(K_MSEC(1000));
+		}
+		printk("Switch to read");
+		while (!nrf24_read(nrf24, buffer, data_len));
 		LOG_HEXDUMP_INF(buffer, data_len, "Received: ");
+		k_sleep(K_MSEC(1000));
+		printk("Switch to write\n");
+	}
+#endif
+#ifdef BOB
+	while (true) {
+		//while(true) {
+		ret = nrf24_read(nrf24, buffer, data_len);
+		LOG_HEXDUMP_INF(buffer, data_len, "Received: ");
+		//}
+		printk("Switch to write\n");
+		strncpy(buffer, "Hi Alice Im Bob", 16);
+		while (true) {
+			k_sleep(K_MSEC(1000));
+			ret = nrf24_write(nrf24, buffer, data_len);
+			printk("Ret: %d\n", ret);
+			if (ret != 0) {
+				printk("Not received or ACK not set\n");
+			} else {
+				printk("Switch to read\n");
+				break;
+			}
+		}
 	}
 #endif
 	return 0;
